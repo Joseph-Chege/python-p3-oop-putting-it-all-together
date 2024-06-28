236834c8d58d36233e8171db5cb1c0b924e3dced
Joseph-Chege <mjchege23@gmail.com>
2024-06-28 14:37:25 +0300

    Used Object Oriented Programming to create instances with attributes and properties

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title=None, page_count=None):
+        self._title = None
+        self._page_count = None
+        if title is not None and page_count is not None:
+            self.title = title
+            self.page_count = page_count
+        else:
+            raise ValueError("title and page_count are required")
+        
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        self._title = value
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+        
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand=None, size=None):
+        self._brand = None
+        self._size = None
+        if brand is not None and size is not None:
+            self.brand = brand
+            self.size = size
+        else:
+            raise ValueError("brand and size are required")
+        
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, value):
+        self._brand = value.title()
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+stan_smith = Shoe('Adidas', 8)
+print(stan_smith.brand)
+print(stan_smith.size)
+print(stan_smith.cobble())
